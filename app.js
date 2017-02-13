1098517dfd9e1da1ded5ab9b7add47c7e3731395
nicky evers <livelyevers@gmail.com>
2017-02-13 14:09:19 -0800

    created basic files and linked, started js

---
+++ app.js
@@ -0,0 +1,19 @@
+var storePike = {
+	minCustomer: 23,
+	maxCustomer: 65,
+	avgCookiesPer: 6.3,
+
+	var randomCustomerCount: function() {
+		return Math.floor(Math.random() * 10)
+	},
+	
+};
+
+var storeAirport = {
+	minCustomer = 3,
+	maxCustomer = 24,
+	avgCookiesPer = 1.2,
+
+	randomCustomerCount(); 
+
+};
