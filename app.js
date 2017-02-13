6523a15f56608f5c56889dabda93bff65e7168b0
nicky evers <livelyevers@gmail.com>
2017-02-13 15:23:41 -0800

    got first document write

--- app.js
+++ app.js
@@ -1,19 +1,65 @@
-var storePike = {
-	minCustomer: 23,
-	maxCustomer: 65,
-	avgCookiesPer: 6.3,
+'use strict';
+var storeHours = ['8:00am', '9:00am', '10:00am', '11:00am', '12:00am', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm'];
 
-	var randomCustomerCount: function() {
-		return Math.floor(Math.random() * 10)
-	},
-	
+var storePike = {
+  minCustomer: 23,
+  maxCustomer: 65,
+  avgCookiesPer: 6.3,
+  store: '1st and Pike',
+
+  randomCustomerCount: function() {
+    return (Math.floor(Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer)) * this.avgCookiesPer;
+
+  },
+
+	cookiesPerHour: function() {
+		for (i = 0; i < storeHours.length; i++) {
+			
+		}
+	}
+
+  printToDom: function() {
+    var storeList = document.getElementById ('store_list');
+    var newH3 = document.createElement ('h3');
+    newH3.textContent = this.store;
+    storeList.appendChild(newH3);
+
+    var newLiHours = document.createElement ('li');
+		newLi.textContent = this.
+		storeList.appendChild(newLiHours);
+  },
+
+
 };
 
-var storeAirport = {
-	minCustomer = 3,
-	maxCustomer = 24,
-	avgCookiesPer = 1.2,
+storePike.printToDom();
 
-	randomCustomerCount(); 
+var storeAirport = {
+  minCustomer: 3,
+  maxCustomer: 24,
+  avgCookiesPer: 1.2,
+
+
+};
+
+var storeSeattleCenter = {
+  minCustomer: 11,
+  maxCustomer: 38,
+  avgCookiesPer: 3.7,
+
+};
+
+var storeCapitol = {
+  minCustomer: 20,
+  maxCustomer: 38,
+  avgCookiesPer: 2.3,
+
+
+};
+
+var storeAlki = {
+  minCustomer: 2,
+  maxCustomer: 16,
+  avgCookiesPer: 4.6,
 
 };
