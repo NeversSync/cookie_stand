aa6681c40f48e08a9605ce233f6d779345a31c1c
nicky evers <livelyevers@gmail.com>
2017-02-16 15:51:01 -0800

    added hourly totals row and cleared form for each render

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 'use strict';
 
 //to do:
-var storeHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm'];
+var storeHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+
+var stores = [
+  new Store(23, 65, 6.3, '1st and Pike'),
+  new Store(3, 24, 1.2, 'Seatac Airport'),
+  new Store(11, 38, 3.7, 'Seattle Center'),
+  new Store(20, 38, 2.3, 'Capitol Hill'),
+  new Store(2, 16, 4.6, 'Alki')
+];
 
 function Store(minCustomer, maxCustomer, avgCookiesPer, store) {//constructing store blueprint
   this.storeName = store;
@@ -17,6 +25,8 @@ Store.prototype.randomCustomerCount = function() {//generating random # of cooki
 };
 
 Store.prototype.cookiesPerHour = function() {//generating total number of cookies per hour
+  this.storeCookiesPerHour = [];
+  this.totalDailyCookies = 0;
   for (var i = 0; i < storeHours.length; i++) {
     var numCookiesPerHour = this.randomCustomerCount();
     this.totalDailyCookies += numCookiesPerHour;
@@ -46,6 +56,8 @@ Store.prototype.renderRow = function(parentTable) {//creating and rendering row 
 function renderTable() {//function to create table structure
   //making thead
   var tableData = document.getElementById('store_data');
+  tableData.textContent = '';
+
   var newTHead = document.createElement('thead');
   tableData.appendChild(newTHead);
   //making tr
@@ -68,11 +80,42 @@ function renderTable() {//function to create table structure
   newTR.appendChild(totalTH);
 
   //rendering and populating each store row
-  pikeStore.renderRow(tableData);
-  airportStore.renderRow(tableData);
-  seattleCenterStore.renderRow(tableData);
-  capitolStore.renderRow(tableData);
-  alkiStore.renderRow(tableData);
+  for(var j = 0; j < stores.length; j++) {
+    stores[j].renderRow(tableData);
+  }
+  renderTableFoot();
+};
+
+var renderTableFoot = function() {
+  var tableData = document.getElementById ('store_data');
+  var hourTotalTFoot = document.createElement('tfoot');
+  tableData.appendChild(hourTotalTFoot);
+
+  var hourTotalTRow = document.createElement('tr');
+  hourTotalTFoot.appendChild(hourTotalTRow);
+
+  var hourTotalTH = document.createElement('th');
+  hourTotalTH.textContent = 'Totals';
+  hourTotalTRow.appendChild(hourTotalTH);
+
+  var hourTotalTD;
+  var hourlyTotal = 0;
+  var dailyTotalFoot = 0;
+
+  for (var i = 0; i < storeHours.length; i++) {
+    hourTotalTD = document.createElement ('td');
+    hourlyTotal = 0;
+
+    for(var j = 0; j < stores.length; j++) {
+      hourlyTotal += stores[j].storeCookiesPerHour[i];
+    }
+    dailyTotalFoot += hourlyTotal;
+    hourTotalTD.textContent = hourlyTotal;
+    hourTotalTRow.appendChild(hourTotalTD);
+  }
+  hourTotalTD = document.createElement ('td');
+  hourTotalTD.textContent = dailyTotalFoot;
+  hourTotalTRow.appendChild(hourTotalTD);
 };
 
 // generating form and assigning inputs to constructor properties
@@ -87,30 +130,14 @@ function submitHandler(event) {
   var maxCustomer = parseInt(event.target.max_customers.value);
   var avgCookiesPer = parseInt(event.target.avg_cookies_per.value);
 
+  event.target.store_title.value = '';
+  event.target.min_customers.value = '';
+  event.target.max_customers.value = '';
+  event.target.avg_cookies_per.value = '';
+
   var newStore = new Store(minCustomer, maxCustomer, avgCookiesPer, storeName);
-  var tableData = document.getElementById('store_data');
-  newStore.renderRow(tableData);
+  stores.push(newStore);
+  renderTable();
 };
 
-// Pushing values to new stores
-var pikeStore = new Store(23, 65, 6.3, '1st and Pike');
-var airportStore = new Store(3, 24, 1.2, 'Seatac Airport');
-var seattleCenterStore = new Store(11, 38, 3.7, 'Seattle Center');
-var capitolStore = new Store(20, 38, 2.3, 'Capitol Hill');
-var alkiStore = new Store(2, 16, 4.6, 'Alki');
-
 renderTable();
-
-
-//finish this; write nested for loop that adds up total cookies per hour and renders that data to new td.
-// var sixAmTotal = pikeStore.storeCookiesPerHour[0] + airportStore.storeCookiesPerHour[0] + seattleCenterStore.storeCookiesPerHour[0] + capitolStore.storeCookiesPerHour[0] + alkiStore.storeCookiesPerHour[0];
-
-// function hourlyTotal() {
-//   var hourTotalData = document.getElementById ('store_data');
-//   var hourTotalTR = document.createElement ('tr');
-//   hourTotalTR.textContent('test');
-//   hourTotalData.appendChild(hourTotalTR);
-//   for (var i = 0; i <
-//   var hourTotalTD = document.createElement ('td');
-//   hourTotalTD.textContent('')
-// }
